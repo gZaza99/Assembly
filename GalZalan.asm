3e753b18cf651321fa12ad5a0387ab250711938e
MrZalan <zalan.gal.99@gmail.com>
2023-01-09 22:05:30 +0100

    Task 4
    Első lépésként tudjunk kényelmesen rajzolni

--- GalZalan.asm
+++ GalZalan.asm
@@ -1,7 +1,11 @@
-.MODEL SMALL
-	Space EQU " "					;Szóköz karakter
-	CR    EQU 13
-	LF    EQU 10
+.MODEL SMALL, C						;Paraméter vermen keresztüli átadásához kell a híváskonvenció
+	Space         EQU " "			;Szóköz karakter
+	CR            EQU 13			;CR-be a kurzor a sor elejére kód
+	LF            EQU 10			;LF-be a kurzor új sorba kód
+	attr_text     EQU 00000111b		;Standard fekete alapon halvány fehér szín
+	attr_border   EQU 01110000b		;Fehér alapon fekete szín
+	Screen_width  EQU 80			;Képernyõ szélessége
+	Screen_height EQU 25			;Képernyõ magassága
 
 .STACK
 .DATA
@@ -19,25 +23,39 @@ main PROC
 	MOV  AX, Dgroup					;DS beállítása
 	MOV  DS, AX
 
-	LEA  BX, disc_text
-	CALL write_string
-	CALL read_decimal
-	MOV  disc, DL
+	MOV  AX, 0B800h					;Képernyõ-memória szegmenscíme ES-be
+	MOV  ES, AX
 
-	LEA  BX, sector_text
-	CALL write_string
-	CALL read_decimal
-	MOV  sector, DL
+	MOV  AL, Screen_height			;Y = 25 - 1
+	DEC  AL
+	MOV  AH, 60						;X = 60
+	PUSH AX							;XY = [ 10 | 1 ]
+	MOV  AL, "P"					;Karakter: "P"
+	MOV  AH, attr_text				;Attribútum: Standard megjelenítés
+	PUSH AX
+	CALL draw_char					;Kiírás
 
-	LEA  BX, block					;DS:BX memóriacímre tölti a blokkot
-	MOV  AL, disc					;Lemezmeghajtó száma (A:0, B:1, C:2, stb.)
-	MOV  CX, 1						;Egyszerre beolvasott blokkok száma
-	XOR  DX, DX
-	MOV  DL, sector					;Lemezolvasás kezdõblokkja
-	INT  25h						;Olvasás
-	POPF							;A veremben tárolt jelzõbitek törlése
-	XOR  DX, DX						;Kiírandó adatok kezdõcíme DS:DX
-	CALL write_block				;Egy blokk kiírása
+	;LEA  BX, disc_text
+	;CALL write_string
+	;CALL read_decimal
+	;MOV  disc, DL
+
+	;LEA  BX, sector_text
+	;CALL write_string
+	;CALL read_decimal
+	;MOV  sector, DL
+
+	;CALL clear_screen
+
+	;LEA  BX, block					;DS:BX memóriacímre tölti a blokkot
+	;MOV  AL, disc					;Lemezmeghajtó száma (A:0, B:1, C:2, stb.)
+	;MOV  CX, 1						;Egyszerre beolvasott blokkok száma
+	;XOR  DX, DX
+	;MOV  DL, sector					;Lemezolvasás kezdõblokkja
+	;INT  25h						;Olvasás
+	;POPF							;A veremben tárolt jelzõbitek törlése
+	;XOR  DX, DX						;Kiírandó adatok kezdõcíme DS:DX
+	;CALL write_block				;Egy blokk kiírása
 
 	MOV  AH, 4Ch					;AH-ba a visszatérés funkciókódja
 	INT  21h						;Visszatérés az operációs rendszerbe
@@ -90,7 +108,7 @@ out_line_visible:
 	RET								;Vissza a hívó programba
 out_line ENDP
 
-write_string proc					;BX-ben címzett karaktersorozat kiírása 0 kódig.
+write_string PROC					;BX-ben címzett karaktersorozat kiírása 0 kódig.
 	PUSH DX							;DX mentése a verembe
 	PUSH BX							;BX mentése a verembe
 write_string_new:
@@ -104,9 +122,9 @@ write_string_end:
 	POP  BX							;BX visszaállítása
 	POP  DX							;DX visszaállítása
 	RET								;Visszatérés
-write_string endp
+write_string ENDP
 
-read_decimal proc
+read_decimal PROC
 	PUSH AX							;AX mentése a verembe
 	PUSH BX							;BX mentése a verembe
 	MOV  BL, 10						;BX-be a számrendszer alapszáma, ezzel szorzunk
@@ -124,9 +142,9 @@ read_decimal_end:
 	POP  BX							;AB visszaállítása
 	POP  AX							;AX visszaállítása
 	RET								;Visszatérés a hívó rutinba
-read_decimal endp
+read_decimal ENDP
 
-write_hexa proc						;A DL-ben lévõ két hexa számjegy kiírása
+write_hexa PROC						;A DL-ben lévõ két hexa számjegy kiírása
 	PUSH CX							;CX mentése a verembe
 	PUSH DX							;DX mentése a verembe
 	MOV  DH, DL						;DL mentése
@@ -139,7 +157,7 @@ write_hexa proc						;A DL-ben lévõ két hexa számjegy kiírása
 	POP  DX							;DX visszaállítása
 	POP  CX							;CX visszaállítása
 	RET								;Visszatérés a hívó rutinba
-write_hexa endp
+write_hexa ENDP
 
 write_hexa_digit PROC
 	PUSH DX							;DX mentése a verembe
@@ -166,29 +184,50 @@ cr_lf ENDP
 clear_screen PROC
 	XOR  AL, AL						;Ablak törlése
 	XOR  CX, CX						;Bal felsõ sarok (CL = 0, CH = 0)
-	MOV  DH, 49						;50 soros képernyõ alsó sora (25 sorosnál ide 24 kell)
-	MOV  DL, 79						;Jobb oldali oszlop sorszáma
+	MOV  DH, Screen_height			;50 soros képernyõ alsó sora (25 sorosnál ide 24) kell
+	DEC  DH							;Képernyõ magassága -1
+	MOV  DL, Screen_width			;Jobb oldali oszlop sorszáma kell
+	DEC  DL							;Képernyõ szélessége -1
 	MOV  BH, 7						;Törölt helyek attribútuma: fekete háttér szürke karakter
 	MOV  AH, 6						;Sorgörgetés felfelé (Scroll-up) funkció
 	INT  10h						;Képernyõ törlése
 	RET
 clear_screen ENDP
 
-read_char proc						;Karakter beolvasása. A beolvasott karakter DL-be kerül
+read_char PROC						;Karakter beolvasása. A beolvasott karakter DL-be kerül
 	PUSH AX							;AX mentése a verembe
 	MOV  AH, 1						;AH-ba a beolvasás funkciókód
 	INT  21h						;Egy karakter beolvasása, a kód AL-be kerül
 	MOV  DL, AL						;DL-be a karakter kódja
 	POP  AX							;AX visszaállítása
 	RET								;Visszatérés a hívó rutinba
-read_char endp
+read_char ENDP
 
-write_char proc						;A DL-ben lévõ karakter kiírása a képernyõre
+;2 * ( (y-1) * <sorhossz=80> + x )
+draw_char PROC USES AX BX DX, MemoryItem: WORD, XY: WORD			;XY = [ X:BYTE | Y:BYTE ]
+	MOV  DX, XY
+	MOV  AL, DL						;AL -be az Y paraméter a verembõl
+	DEC  AL							;Y-1 a nullától indexelés miatt
+	MOV  BL, Screen_width			;BL -be a képernyõ szélessége (oszlopok száma)
+	MUL  BL							;AX -be a kívánt magasság
+	XOR  BX, BX						;BX kinullázása a felsõ 8 bit miatt
+	MOV  BL, DH						;BL -be az X paraméter a verembõl
+	ADD  AX, BX						;AX -be a kívánt offset karaktter pozíció
+	SHL  AX, 1						;AX -be a kívánt offset memória cím
+	
+	MOV  BX, AX						;BX -be a kívánt offset memória cím, mert csak bázis, vagy index regiszter elfogadott
+	MOV  AX, MemoryItem				;AX -be a kirajzolni kívánt karakter
+
+	MOV  ES:[BX], AX				;Karakter kirajzolása
+	RET
+draw_char ENDP
+
+write_char PROC						;A DL-ben lévõ karakter kiírása a képernyõre
 	PUSH AX							;AX mentése a verembe
 	MOV  AH, 2						; AH-ba a képernyõre írás funkciókódja
 	INT  21h						; Karakter kiírása
 	POP  AX							;AX visszaállítása
 	RET								;Visszatérés a hívó rutinba
-write_char endp
+write_char ENDP
 
 END main
